0abf0b73d18b6fd6061983e30d60db9f3b2a9ce8
Renan Vargas <renanspvargas@outlook.com>
2022-05-26 18:19:50 -0300

    Adding custom app bar to the Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:masterclass_app/components/app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
+    return const Scaffold(
+      appBar: CustomAppBar(title: "Atividades"),
+      body: Center(
         child: Text("Home"),
       ),
     );
