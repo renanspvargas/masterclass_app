d45690d9196c659c7a5d62dc130b7c471cfbbc3c
Renan Vargas <renanspvargas@outlook.com>
2022-05-26 16:47:09 -0300

    creating basic theme and using on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:masterclass_app/Navigation/navigation_manager.dart';
+import 'package:masterclass_app/navigation/navigation_constants.dart';
 import 'package:masterclass_app/screens/home_screen.dart';
 import 'package:masterclass_app/screens/profile_screen.dart';
 import 'package:masterclass_app/screens/repos_screen.dart';
 import 'package:masterclass_app/screens/splash_screen.dart';
+import 'package:masterclass_app/theme/theme_constants.dart';
+import 'package:masterclass_app/theme/theme_manager.dart';
 
 void main() {
   runApp(const AppStart());
@@ -16,11 +18,14 @@ class AppStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeManager.shared.themeMode,
       routes: <String, WidgetBuilder>{
-        NavigationManager.shared.root: (context) => const SplashScreen(),
-        NavigationManager.shared.home: (context) => const HomeScreen(),
-        NavigationManager.shared.repos: (context) => const ReposScreen(),
-        NavigationManager.shared.profile: (context) => const ProfileScreen(),
+        NavigationConstans.shared.root: (context) => const SplashScreen(),
+        NavigationConstans.shared.home: (context) => const HomeScreen(),
+        NavigationConstans.shared.repos: (context) => const ReposScreen(),
+        NavigationConstans.shared.profile: (context) => const ProfileScreen(),
       },
     );
   }
---
+++ lib/theme/theme_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeManager with ChangeNotifier {
+  static ThemeManager shared = ThemeManager();
+
+  ThemeMode _themeMode = ThemeMode.light;
+
+  get themeMode => _themeMode;
+
+  toggleTheme(bool isDark) {
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+  }
+}
