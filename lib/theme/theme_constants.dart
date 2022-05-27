d55b6d874f62bacd6107b0066f117f55a47bc0dd
Renan Vargas <renanspvargas@outlook.com>
2022-05-27 20:24:46 -0300

    Adding background color to theme

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -2,8 +2,10 @@ import 'package:masterclass_app/imports.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
+  scaffoldBackgroundColor: const Color(0xffD6DFE4),
 );
 
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
+  scaffoldBackgroundColor: const Color(0xff172026),
 );
