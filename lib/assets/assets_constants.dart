f4fefabc62d8848bf695171098c4f32cb3b7f483
Renan Vargas <renanspvargas@outlook.com>
2022-05-27 18:48:34 -0300

    Adding custom fonts

--- lib/assets/assets_constants.dart
+++ lib/assets/assets_constants.dart
@@ -1,7 +1,9 @@
+import 'fonts.dart';
 import 'images.dart';
 import 'svgs.dart';
 
 class AssetsConstants {
   static Images images = Images();
   static Svgs svgs = Svgs();
+  static Fonts fonts = Fonts();
 }
---
+++ lib/assets/fonts.dart
@@ -0,0 +1,7 @@
+class Fonts {
+  final _montserrat = 'Montserrat';
+  final _poppins = 'Poppins';
+
+  String get montserrat => _montserrat;
+  String get poppins => _poppins;
+}
