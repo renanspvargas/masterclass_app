a15c107d86152f8718215fc5245066512bd95425
Renan Vargas <renanspvargas@outlook.com>
2022-05-30 21:07:00 -0300

    adding colors and text to theme constants

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -2,10 +2,24 @@ import 'package:masterclass_app/imports.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  scaffoldBackgroundColor: const Color(0xffD6DFE4),
+  scaffoldBackgroundColor: scaffoldBackgrondLight,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: scaffoldBackgrondLight,
+  ),
+  textTheme: TextTheme(
+    headline6: appBarTitle.copyWith(color: textHighlightLight),
+    subtitle1: appBarSubTitle.copyWith(color: textHighlightLight),
+  ),
 );
 
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
-  scaffoldBackgroundColor: const Color(0xff172026),
+  scaffoldBackgroundColor: scaffoldBackgrondDark,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: scaffoldBackgrondDark,
+  ),
+  textTheme: TextTheme(
+    headline6: appBarTitle.copyWith(color: textHighlightDark),
+    subtitle1: appBarSubTitle.copyWith(color: textHighlightDark),
+  ),
 );
