4ef65ae42983e757e2af7e2ee4f12c93d75d3db2
Renan Vargas <renanspvargas@outlook.com>
2022-06-01 21:55:33 -0300

    creating singleton for theme manager

--- lib/theme/theme_manager.dart
+++ lib/theme/theme_manager.dart
@@ -1,7 +1,9 @@
 import 'package:masterclass_app/imports.dart';
 
 class ThemeManager with ChangeNotifier {
-  static ThemeManager shared = ThemeManager();
+  ThemeManager._internal();
+  static final ThemeManager _instance = ThemeManager._internal();
+  static ThemeManager get shared => _instance;
 
   ThemeMode _themeMode = ThemeMode.dark;
   var _isDark = true;
