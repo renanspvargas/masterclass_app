3d3ba318ce11208f95116ee06b852b021381ecb4
renan vargas <renanspvargas@outlook.com>
2022-06-04 15:49:16 -0300

    adding activities data

--- lib/data/activities_data.dart
+++ lib/data/activities_data.dart
@@ -4,12 +4,34 @@ class ActivitiesData {
   static List<Activity> allActivities() {
     return [
       Activity(
+        icon: AssetsConstants.svgs.awesomeBrain,
+        title: "Lógica",
+        body: "Algoritmo para validação de CPF",
+        exercicios: 2,
+        sourceLink: "",
+      ),
+      Activity(
         icon: AssetsConstants.svgs.awesomeRunning,
         title: "Animações",
         body: "Estudos sobre animações implícitas e controladas",
         exercicios: 4,
-        sourceLink: "sourceLink",
+        sourceLink: "",
       ),
+      Activity(
+        icon: AssetsConstants.svgs.awesomeGlasses,
+        title: "Leitura de Mockup",
+        body:
+            "Aplicação da técnica de leitura de mockup, contendo 2 exercícios",
+        exercicios: 2,
+        sourceLink: "",
+      ),
+      Activity(
+        icon: AssetsConstants.svgs.materialToys,
+        title: "Playground",
+        body: "Ambiente destinado a testes e estudos em geral",
+        exercicios: 0,
+        sourceLink: "",
+      )
     ];
   }
 }
