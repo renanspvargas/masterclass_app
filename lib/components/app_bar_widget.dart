efca522bcf5f7a7b02c3c067583e3efc156aecbe
Renan Vargas <renanspvargas@outlook.com>
2022-06-03 21:44:57 -0300

    Correcting app bar color

--- lib/components/app_bar_widget.dart
+++ lib/components/app_bar_widget.dart
@@ -11,42 +11,45 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          children: [
-            Image.asset(
-              AssetsConstants.images.masterclassIcon,
-              width: 48,
-              height: 48,
-            ),
-            const SizedBox(width: 4),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                Text(
-                  "Flutterando Masterclass",
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-              ],
-            ),
-            const Expanded(child: SizedBox()),
-            GestureDetector(
-              child: SvgPicture.asset(
-                AssetsConstants.svgs.awesomeMoon,
-                color: const Color(0xffEDF4F8),
-                semanticsLabel: 'Toggle Theme',
+      child: Container(
+        color: Theme.of(context).appBarTheme.backgroundColor,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Image.asset(
+                AssetsConstants.images.masterclassIcon,
+                width: 48,
+                height: 48,
               ),
-              onTap: () {
-                ThemeManager.shared.toggleTheme();
-              },
-            )
-          ],
+              const SizedBox(width: 4),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  Text(
+                    "Flutterando Masterclass",
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                ],
+              ),
+              const Expanded(child: SizedBox()),
+              GestureDetector(
+                child: SvgPicture.asset(
+                  AssetsConstants.svgs.awesomeMoon,
+                  color: const Color(0xffEDF4F8),
+                  semanticsLabel: 'Toggle Theme',
+                ),
+                onTap: () {
+                  ThemeManager.shared.toggleTheme();
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
