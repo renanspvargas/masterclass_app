08c9ee93f50e7f7eeb08a0f4eed0aad42554aebf
renan vargas <renanspvargas@outlook.com>
2022-06-06 16:43:32 -0300

    adding app bar for profile and repos screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,6 +5,8 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Sobre o dev'),
+    );
   }
 }
--- lib/screens/repos_screen.dart
+++ lib/screens/repos_screen.dart
@@ -5,6 +5,8 @@ class ReposScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Repositórios'),
+    );
   }
 }
