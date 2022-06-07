6a6f0a89916e321142f2b74144ab6cb85e5c6709
renan vargas <renanspvargas@outlook.com>
2022-06-07 15:24:56 -0300

    creating model for profile card

---
+++ lib/models/profile_card_model.dart
@@ -0,0 +1,19 @@
+class ProfileCard {
+  final String image;
+  final String name;
+  final String description;
+  final String telegramLink;
+  final String githubLink;
+  final String linkedinLink;
+  final String instagramLink;
+
+  ProfileCard(
+    this.image,
+    this.name,
+    this.description,
+    this.telegramLink,
+    this.githubLink,
+    this.linkedinLink,
+    this.instagramLink,
+  );
+}
