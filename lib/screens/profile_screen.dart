8755b8426c693767f64057e9f8c2f14efce932d7
renan vargas <renanspvargas@outlook.com>
2022-06-06 22:23:47 -0300

    basic card for profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,8 +5,81 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Sobre o dev'),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Sobre o dev'),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).listTileTheme.tileColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        shape: BoxShape.circle,
+                      ),
+                      height: 120,
+                      width: 120,
+                    ),
+                    Text(
+                      'Renan S. P. Vargas',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        'Duis rhoncus dui venenatis consequat porttitor. Etiam aliquet congue consequat. In posuere, nunc sit amet laoreet blandit, urna sapien.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          color: Colors.indigo,
+                          width: 20,
+                          height: 20,
+                        ),
+                        const SizedBox(width: 27),
+                        Container(
+                          color: Colors.indigo,
+                          width: 20,
+                          height: 20,
+                        ),
+                        const SizedBox(width: 27),
+                        Container(
+                          color: Colors.indigo,
+                          width: 20,
+                          height: 20,
+                        ),
+                        const SizedBox(width: 27),
+                        Container(
+                          color: Colors.indigo,
+                          width: 20,
+                          height: 20,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
