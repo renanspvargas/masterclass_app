71fa137f828ceeb430c41f70e6adc2df29a93dc6
renan vargas <renanspvargas@outlook.com>
2022-06-08 15:22:19 -0300

    adding data for profile card data class

--- lib/data/profile_card_data.dart
+++ lib/data/profile_card_data.dart
@@ -1,3 +1,16 @@
 import 'package:masterclass_app/imports.dart';
 
-class ProfileCardData {}
+abstract class ProfileCardData {
+  static ProfileCardModel data() {
+    return ProfileCardModel(
+      image: AssetsConstants.images.profilePicture,
+      name: "Renan S. P. Vargas",
+      description:
+          "I'm a iOS developer working with Swift since jan/2021. In 2020 I worked a year as Data Engineer specialized on AWS",
+      telegramLink: "https://t.me/renanspvargas",
+      githubLink: "https://github.com/renanspvargas",
+      linkedinLink: "https://www.linkedin.com/in/renanspvargas/",
+      instagramLink: "https://www.instagram.com/renanspvargas/",
+    );
+  }
+}
