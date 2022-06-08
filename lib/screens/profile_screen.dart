d39adc274eceb381b9f723bad83e94bbff036ca3
renan vargas <renanspvargas@outlook.com>
2022-06-08 15:25:09 -0300

    separating profile card to its own widget; dynamically distributing data

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:masterclass_app/imports.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  ProfileScreen({Key? key}) : super(key: key);
+
+  final ProfileScreenModel profileData = ProfileScreenData.data();
 
   @override
   Widget build(BuildContext context) {
@@ -15,67 +17,7 @@ class ProfileScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).listTileTheme.tileColor,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: MediaQuery.of(context).size.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.amber,
-                        shape: BoxShape.circle,
-                      ),
-                      height: 120,
-                      width: 120,
-                    ),
-                    Text(
-                      'Renan S. P. Vargas',
-                      style: Theme.of(context).textTheme.headline2,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        'Duis rhoncus dui venenatis consequat porttitor. Etiam aliquet congue consequat. In posuere, nunc sit amet laoreet blandit, urna sapien.',
-                        style: Theme.of(context).textTheme.bodyText1,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          color: Colors.indigo,
-                          width: 20,
-                          height: 20,
-                        ),
-                        const SizedBox(width: 27),
-                        Container(
-                          color: Colors.indigo,
-                          width: 20,
-                          height: 20,
-                        ),
-                        const SizedBox(width: 27),
-                        Container(
-                          color: Colors.indigo,
-                          width: 20,
-                          height: 20,
-                        ),
-                        const SizedBox(width: 27),
-                        Container(
-                          color: Colors.indigo,
-                          width: 20,
-                          height: 20,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+              ProfileCardWidget(profileCard: profileData.profileCard),
               const SizedBox(height: 20),
               Text(
                 'Tecnologias Favoritas',
@@ -95,6 +37,85 @@ class ProfileScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ProfileCardWidget extends StatelessWidget {
+  final ProfileCardModel profileCard;
+  const ProfileCardWidget({
+    Key? key,
+    required this.profileCard,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).listTileTheme.tileColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.amber,
+              shape: BoxShape.circle,
+            ),
+            height: 120,
+            width: 120,
+            child: ClipOval(
+              child: Image.asset(
+                profileCard.image,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Text(
+            profileCard.name,
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              profileCard.description,
+              style: Theme.of(context).textTheme.bodyText1,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.indigo,
+                width: 20,
+                height: 20,
+              ),
+              const SizedBox(width: 27),
+              Container(
+                color: Colors.indigo,
+                width: 20,
+                height: 20,
+              ),
+              const SizedBox(width: 27),
+              Container(
+                color: Colors.indigo,
+                width: 20,
+                height: 20,
+              ),
+              const SizedBox(width: 27),
+              Container(
+                color: Colors.indigo,
+                width: 20,
+                height: 20,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
