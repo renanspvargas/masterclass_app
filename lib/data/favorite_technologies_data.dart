dd6e5984f24c4f3bed6f263b77656875be5c7469
renan vargas <renanspvargas@outlook.com>
2022-06-13 16:23:39 -0300

    adding data for favorite technologies

--- lib/data/favorite_technologies_data.dart
+++ lib/data/favorite_technologies_data.dart
@@ -2,6 +2,19 @@ import 'package:masterclass_app/imports.dart';
 
 abstract class FavoriteTechnologiesData {
   static List<FavoriteTechnologyModel> allTechnologies() {
-    return [];
+    return [
+      FavoriteTechnologyModel(
+        image: AssetsConstants.svgs.techFlutter,
+        name: "Flutter",
+      ),
+      FavoriteTechnologyModel(
+        image: AssetsConstants.svgs.techPython,
+        name: "Python",
+      ),
+      FavoriteTechnologyModel(
+        image: AssetsConstants.svgs.techSwift,
+        name: "Swift",
+      ),
+    ];
   }
 }
