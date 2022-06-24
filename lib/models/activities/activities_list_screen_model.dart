25a0c4a158f811ca83223166371417cf85adb020
renan vargas <renanspvargas@outlook.com>
2022-06-24 00:07:56 -0300

    creating model for exercises

---
+++ lib/models/activities/activities_list_screen_model.dart
@@ -0,0 +1,11 @@
+import 'package:masterclass_app/imports.dart';
+
+class ActivitiesListScreenModel {
+  final String title;
+  final List<ActivityPageModel> activities;
+
+  ActivitiesListScreenModel({
+    required this.title,
+    required this.activities,
+  });
+}
---
+++ lib/models/activities/activity_page_model.dart
@@ -0,0 +1,9 @@
+class ActivityPageModel {
+  final String title;
+  final String navigator;
+
+  ActivityPageModel({
+    required this.title,
+    required this.navigator,
+  });
+}
