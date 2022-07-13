922cd160a4af20a70ea834e16de698f68dafd789
renan vargas <renanspvargas@outlook.com>
2022-07-13 19:05:14 -0300

    adding cpf screen to imports file

--- lib/screens/activities/activities_path.dart
+++ lib/screens/activities/activities_path.dart
@@ -6,3 +6,6 @@ export 'package:masterclass_app/screens/activities/mockup/tinder_app.dart';
 export 'package:masterclass_app/screens/activities/animation/exercicio1.dart';
 export 'package:masterclass_app/screens/activities/animation/exercicio2.dart';
 export 'package:masterclass_app/screens/activities/animation/exercicio3.dart';
+
+// CPF Validator
+export 'package:masterclass_app/screens/activities/logic/cpf_validator_screen.dart';
