0bda78d86057da1041f165033fae2abbd384f5b2
renanspvargas <renanspvargas@outlook.com>
2023-01-18 16:44:13 -0300

    navigation refactor: creating onGeneratedRoute

---
+++ lib/Navigation/app_router.dart
@@ -0,0 +1,58 @@
+import 'package:masterclass_app/imports.dart';
+import 'package:masterclass_app/Navigation/app_routes.dart';
+
+class AppRouter {
+  static Route<dynamic>? onGeneratedRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case AppRoutes.splashScreen:
+        return MaterialPageRoute(builder: ((context) => const SplashScreen()));
+      case AppRoutes.homeScreen:
+        return MaterialPageRoute(builder: ((context) => HomeScreen()));
+      case AppRoutes.repoScreen:
+        return MaterialPageRoute(builder: ((context) => const ReposScreen()));
+      case AppRoutes.profileScreen:
+        return MaterialPageRoute(builder: ((context) => ProfileScreen()));
+
+      case AppRoutes.mockupMoneyApp:
+        return MaterialPageRoute(builder: ((context) => const MoneyApp()));
+      case AppRoutes.mockupTinderApp:
+        return MaterialPageRoute(builder: ((context) => const TinderApp()));
+
+      case AppRoutes.implicitButtonAnimation:
+        return MaterialPageRoute(
+            builder: ((context) => const AnimatedButtonScreen()));
+      case AppRoutes.implicitListAnimation:
+        return MaterialPageRoute(
+            builder: ((context) => const AnimatedExpansionTile()));
+      case AppRoutes.controlledButtonAnimation:
+        return MaterialPageRoute(
+            builder: ((context) => const AnimatedExpansionTile()));
+
+      case AppRoutes.cpfValidator:
+        return MaterialPageRoute(
+            builder: ((context) => const AnimatedExpansionTile()));
+
+      case AppRoutes.designPattern:
+        DesingPatternScreenActivity activity =
+            routeSettings.arguments as DesingPatternScreenActivity;
+        return MaterialPageRoute(
+          builder: ((context) => DesingPatternCommonScreen(
+                activity: activity,
+              )),
+        );
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (context) {
+      return Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: Text("Page not found!"),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/Navigation/app_routes.dart
@@ -0,0 +1,17 @@
+class AppRoutes {
+  static const splashScreen = "/splashScreen";
+  static const homeScreen = "/homeScreen";
+  static const repoScreen = "/repoScreen";
+  static const profileScreen = "/profileScreen";
+
+  static const mockupMoneyApp = '/mockup/money';
+  static const mockupTinderApp = '/mockup/tinder';
+
+  static const implicitButtonAnimation = '/animation/exercicio1';
+  static const implicitListAnimation = '/animation/exercicio2';
+  static const controlledButtonAnimation = '/animation/exercicio3';
+
+  static const cpfValidator = '/logic/cpf_validator_screen';
+
+  static const designPattern = '/designPattern/designPattern';
+}
