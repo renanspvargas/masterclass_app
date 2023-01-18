e17b17f4a4076eee47d32dfd0687fe6b37f47a3e
renanspvargas <renanspvargas@outlook.com>
2023-01-18 16:42:34 -0300

    using design pattern activity type as required

--- lib/screens/activities/design_pattern/desing_pattern_common_screen.dart
+++ lib/screens/activities/design_pattern/desing_pattern_common_screen.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:masterclass_app/screens/activities/design_pattern/desing_pattern_common_model.dart';
 
 class DesingPatternCommonScreen extends StatelessWidget {
+  final DesingPatternScreenActivity activity;
   final List<DesignPatternCommonModel> result = [
     DesignPatternCommonModel(id: "1", name: "Teste1"),
     DesignPatternCommonModel(id: "2", name: "Teste2"),
     DesignPatternCommonModel(id: "3", name: "Teste3"),
   ];
 
-  DesingPatternCommonScreen({super.key});
+  DesingPatternCommonScreen({required this.activity, super.key});
 
   @override
   Widget build(BuildContext context) {
