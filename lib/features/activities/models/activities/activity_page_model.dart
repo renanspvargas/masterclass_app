e4825761acf45e794d5667193489a9c9b23e02af
renanspvargas <renanspvargas@outlook.com>
2023-01-21 19:41:20 -0300

    adding args to activity page model

--- lib/features/activities/models/activities/activity_page_model.dart
+++ lib/features/activities/models/activities/activity_page_model.dart
@@ -1,9 +1,11 @@
 class ActivityPageModel {
   final String title;
   final String navigator;
+  final Object? args;
 
   ActivityPageModel({
     required this.title,
     required this.navigator,
+    this.args,
   });
 }
