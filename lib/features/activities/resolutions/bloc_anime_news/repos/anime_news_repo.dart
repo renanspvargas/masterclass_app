6997bcbefe853c95af9559aba0e3c128e47f23e1
renanspvargas <renanspvargas@outlook.com>
2023-01-23 18:35:56 -0300

    creating anime news repo

---
+++ lib/features/activities/resolutions/bloc_anime_news/repos/anime_news_repo.dart
@@ -0,0 +1,19 @@
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/datasource/anime_news_remote_datasource.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/models/anime_news_model.dart';
+
+class AnimeNewsRepository {
+  final AnimeNewsRemoteDataSource _remoteDataSource;
+
+  AnimeNewsRepository([AnimeNewsRemoteDataSource? remoteDataSource])
+      : _remoteDataSource = remoteDataSource ?? AnimeNewsRemoteDataSource();
+
+  Future<List<AnimeNewsModel>> getNews({
+    required int page,
+    int perPage = 10,
+  }) async {
+    final json = await _remoteDataSource.getNews(page: page, perPage: perPage);
+    final List<AnimeNewsModel> news =
+        json.map((e) => AnimeNewsModel.fromJson(e)).toList();
+    return news;
+  }
+}
