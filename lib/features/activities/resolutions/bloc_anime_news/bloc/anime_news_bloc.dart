c6f4c66df119412bef65af0f22e0c4b347455b1f
renanspvargas <renanspvargas@outlook.com>
2023-01-23 19:29:42 -0300

    creating bloc for anime news

---
+++ lib/features/activities/resolutions/bloc_anime_news/bloc/anime_news_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/bloc/anime_news_events.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/bloc/anime_news_states.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/errors/anime_news_errors.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/repos/anime_news_repo.dart';
+
+class AnimeNewsBloc extends Bloc<AnimeNewsEvent, AnimeNewsState> {
+  final AnimeNewsRepository _repo;
+  int _currentPage = 0;
+
+  AnimeNewsBloc([repo])
+      : _repo = repo ?? AnimeNewsRepository(),
+        super(AnimeNewsEmptyState()) {
+    on<GetMoreAnimeNewsEvent>(_getMoreAnimeNewsEvent);
+    on<GetAnimeNewsEvent>(_getAnimeNewsEvent);
+  }
+
+  Future<void> _getAnimeNewsEvent(event, emit) async {
+    emit(AnimeNewsLoadingState());
+
+    try {
+      final news = await _repo.getNews(page: _currentPage);
+      emit(AnimeNewsLoadedState(news));
+    } on AnimeNewsOffilineException catch (e) {
+      emit(AnimeNewsErrorState(e.message));
+    }
+  }
+
+  Future<void> _getMoreAnimeNewsEvent(event, emit) async {
+    _currentPage = _currentPage + 1;
+    _getAnimeNewsEvent(event, emit);
+  }
+}
