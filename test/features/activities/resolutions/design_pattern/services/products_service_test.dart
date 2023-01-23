5a57a716f385eef582bf1990dca2c43ba27012b2
renanspvargas <renanspvargas@outlook.com>
2023-01-22 22:23:32 -0300

    creating test for offiline exception

--- test/features/activities/resolutions/design_pattern/services/products_service_test.dart
+++ test/features/activities/resolutions/design_pattern/services/products_service_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
+import 'package:masterclass_app/features/activities/resolutions/design_pattern/errors/products_errors.dart';
 import 'package:masterclass_app/imports.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -33,6 +34,19 @@ void main() async {
     expect(result.length, 3);
     expect(result.first.id, "0");
     expect(result.first.name, "Flutter 2");
+  }));
+
+  test("Should throw ProductOffilineException when not connected to internet",
+      (() async {
+    final response = Response('', 404);
+
+    when(
+      () => client.get(any()),
+    ).thenAnswer(
+      (_) async => response,
+    );
+
+    expect(service.getProducts, throwsA(isA<ProductOffilineException>()));
   }));
 }
 
