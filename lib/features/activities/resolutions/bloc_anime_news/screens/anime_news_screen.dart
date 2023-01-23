b62ccac4ed112031d5ff4956f997d858490b2756
renanspvargas <renanspvargas@outlook.com>
2023-01-23 19:51:59 -0300

    anime news screen created

---
+++ lib/features/activities/resolutions/bloc_anime_news/screens/anime_news_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/bloc/anime_news_bloc.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/bloc/anime_news_events.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/bloc/anime_news_states.dart';
+import 'package:masterclass_app/imports.dart';
+
+class AnimeNewsScreen extends StatefulWidget {
+  const AnimeNewsScreen({super.key});
+
+  @override
+  State<AnimeNewsScreen> createState() => _AnimeNewsScreenState();
+}
+
+class _AnimeNewsScreenState extends State<AnimeNewsScreen> {
+  final _bloc = AnimeNewsBloc();
+
+  @override
+  void initState() {
+    _bloc.add(GetAnimeNewsEvent());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: BlocBuilder<AnimeNewsBloc, AnimeNewsState>(
+        builder: (context, state) {
+          Widget child = Container();
+
+          if (state is AnimeNewsErrorState) {
+            child = Row(
+              children: [
+                Text(state.message),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    _bloc.add(GetAnimeNewsEvent());
+                  },
+                  child: const Text('tentar novamente'),
+                ),
+              ],
+            );
+          }
+
+          if (state is AnimeNewsLoadingState) {
+            child = const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (state is AnimeNewsLoadedState) {
+            child = ListView.builder(
+              itemCount: state.news.length,
+              itemBuilder: (_, index) {
+                return ListTile(
+                  title: Text(state.news[index].title),
+                );
+              },
+            );
+          }
+
+          if (state is AnimeNewsLoadedMoreState) {
+            child = const Center(
+              child: Text('To be implmented'),
+            );
+          }
+
+          return child;
+        },
+      ),
+    );
+  }
+}
