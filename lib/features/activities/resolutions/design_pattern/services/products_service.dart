bac8b19d0ec19a6e71b4f84814387ee3bf057436
renanspvargas <renanspvargas@outlook.com>
2023-01-22 22:00:00 -0300

    changing Products Service to dependecy injection

--- lib/features/activities/resolutions/design_pattern/services/products_service.dart
+++ lib/features/activities/resolutions/design_pattern/services/products_service.dart
@@ -1,13 +1,17 @@
 import 'package:masterclass_app/imports.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class ProductsService {
+  final Client _client;
+
+  ProductsService([client]) : _client = client ?? Client();
+
   // Explanation for this ip is in the readme
   final _url = 'http://10.0.2.2:3031/products';
 
   Future<List<DesignPatternCommonModel>> getProducts() async {
     try {
-      final response = await http.get(Uri.parse(_url));
+      final response = await _client.get(Uri.parse(_url));
       final json = jsonDecode(response.body) as List;
       List<DesignPatternCommonModel> products =
           json.map((e) => DesignPatternCommonModel.fromJson(e)).toList();
