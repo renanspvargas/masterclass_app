91a836bab9a5c9b024e2409de60ececa49c94ceb
renanspvargas <renanspvargas@outlook.com>
2023-01-23 21:15:44 -0300

    adding verification for api errors

--- lib/features/activities/resolutions/bloc_anime_news/datasource/anime_news_remote_datasource.dart
+++ lib/features/activities/resolutions/bloc_anime_news/datasource/anime_news_remote_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'package:masterclass_app/features/activities/resolutions/bloc_anime_news/errors/anime_news_errors.dart';
 import 'package:masterclass_app/imports.dart';
 
 class AnimeNewsRemoteDataSource {
@@ -11,6 +12,13 @@ class AnimeNewsRemoteDataSource {
   Future<List> getNews({required int page, required int perPage}) async {
     final completePath = '$_path&page=$page&per_page=$perPage';
     final response = await _client.get(Uri.parse(completePath));
+
+    if (response.statusCode == 404) {
+      throw AnimeNewsOffilineException('404');
+    } else if (response.statusCode == 400) {
+      throw AnimeNewsBadRequestException('400');
+    }
+
     final json = jsonDecode(response.body) as List;
     return json;
   }
